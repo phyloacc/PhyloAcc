f045ed87571e33b11e33cd1be82282ace7458d2b
xyz111131 <xyz111131@gmail.com>
2018-09-16 12:57:10 -0400

    Update ratite simulation

---
+++ Simulation_ratite/PAML/read_result.sh
@@ -0,0 +1,5 @@
+for k in {1..8}
+do
+  grep "(((((((((((((((taeGut:" simu_500_200_diffr_2-${k} >simu_500_200_diffr_2-${k}.trees
+done
+grep "(((((((((((((((taeGut:" simu_500_200_diffr_1-1 >simu_500_200_diffr_1-1.trees
