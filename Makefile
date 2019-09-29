6d3dd78c44ad98e8b245abe67f5bbaef25bd494d
beyondpie <panshi.pie@gmail.com>
2019-09-28 22:11:29 -0400

    add install_mac shell script. reduce makefile.

--- Makefile
+++ Makefile
@@ -1,34 +1,23 @@
 TARGET=PhyloAcc
-ifeq ($(shell uname),Darwin)
-	CXX=g++
-else
-	CXX=g++
-endif
+# ifeq ($(shell uname),Darwin)
+# 	CXX=g++
+# else
+# 	CXX=g++
+# endif
+CXX=g++
 
 CFLAGS=-Wall -g -O2 -std=c++11
 LDFLAGS=-lgsl -lm -lgslcblas -larmadillo -fopenmp
-
-GSL_INCLUDE=/usr/local/include/
-GSL_LIB=/usr/local/lib/
-
-
 
 # FIXME: set variables in shell works. in makefile, cannot recognize the pipline, and only return the first command
 # CONDA_ENV_PATH := $(shell conda info -e | grep "\*" | awk -F '[[:space:]]+' '{print $3}')
 # CONDA_ENV_INCLUDE := ${CONDA_ENV_PATH}/include/
 # CONDA_ENV_LIB := ${CONDA_ENV_PATH}/lib/
 
-# FIXME: Hard code the conda include and lib
-CONDA_ENV_INCLUDE=/Users/beyondpie/miniconda3/envs/phyloacc/include/
-CONDA_ENV_LIB=/Users/beyondpie/miniconda3/envs/phyloacc/lib/
 SRC_DIR=SRC
 SRCS=$(SRC_DIR)/*.cpp
 INCLUDES=$(SRC_DIR)/*.h $(SRC_DIR)/*.hpp
 PREFIX=/usr/local
-
-# TODO: delete below, since all the dependencies from conda.
-# $(TARGET): $(SRCS) $(INCLUDES)
-	# $(CXX) $(CFLAGS) -I$(GSL_INCLUDE) -L$(GSL_LIB) $(SRCS) -o $(TARGET) $(LDFLAGS)
 
 $(TARGET): $(SRCS) $(INCLUDES)
 	$(CXX) $(CFLAGS) -I${CONDA_ENV_INCLUDE} -L${CONDA_ENV_LIB} $(SRCS) -o $(TARGET) $(LDFLAGS)
@@ -37,9 +26,9 @@ $(TARGET): $(SRCS) $(INCLUDES)
 # install: $(TARGET)
 # 	cp $< $(DESTDIR)$(PREFIX)/bin/$(TARGET)
 
-.PHONY: uninstall
-uninstall:
-	rm -f $(DESTDIR)$(PREFIX)/bin/$(TARGET)
+# .PHONY: uninstall
+# uninstall:
+# 	rm -f $(DESTDIR)$(PREFIX)/bin/$(TARGET)
 
 .PHONY: clean
 clean:
---
+++ install_mac.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Install cmake, gcc, conda, and git.
+# We suggest you use brew to install cmake and gcc
+brew install cmake
+brew install gcc
+
+# You can install install conda from their website.
+# We use conda as the basic vitural enviorment for the package.
+
+# Install third party package: armadillo, openmp
+# Note: when install armadillo, the system might say warning,
+# which should be OK.
+brew install armadillo
+brew install openmp
+
+# Git clone the repo to your local path.
+# The command below will download the repo in your current path.
+git clone https://github.com/xyz111131/PhyloAcc.git
+# Enter this directory
+cd PhyloAcc
+
+# Create the conda env for phyloacc
+conda create -n phyloacc
+# enter that env
+source activate phyloacc
+
+# Install gsl
+# Note: in this way, we install gsl only for this env.
+conda install gsl
+
+# get conda env path
+export CONDA_ENV_PATH=$(conda info -e | grep "\*" | awk -F '[[:space:]]+' '{print $3}')
+export LD_LIBRARY_PATH=${CONDA_ENV_PATH}/lib/:$LD_LIBRARY_PATH
+export CONDA_ENV_INCLUDE=${CONDA_ENV_PATH}/include/
+export CONDA_ENV_LIB=${CONDA_ENV_PATH}/lib/
+# export LD_RUN_PATH=${CONDA_ENV_PATH}/lib/:$LD_RUN_PATH
+
+# Check if the variable below has value by print it.
+echo ${CONDA_ENV_PATH}
+
+# Even in macOS, we use g++ to compile (not clnag).
+# In makefile, we use g++, so if you have no g++ in your local bin path,
+# you can use soft link below. (Ideally, after brewinng install gcc, it will
+# link gcc to the brew installed dir, i.e., /usr/local/Cellar/gcc).
+ln -s /usr/local/Cellar/gcc/9.2.0/bin/g++-9 /usr/local/bin/g++
+
+# Now, compile the program.
+make
+# Done. A local executable file called PhyloAcc should in the current directory.
