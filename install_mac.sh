859577473c49b8147fdc5bcb0490985c6cf551aa
beyondpie <panshi.pie@gmail.com>
2019-09-28 23:09:17 -0400

    minor, correct syntax.

--- install_mac.sh
+++ install_mac.sh
@@ -32,19 +32,19 @@ conda install -c conda-forge openmp
 # get conda env path
 # Note: please check this value for yourself.
 export CONDA_ENV_PATH=$(conda info -e | grep "\*" | awk -F '[[:space:]]+' '{print $3}')
-# LD_LIBRARY_PARH is used for the program to find the library when it runs.
+# LD_LIBRARY_PARH is used for the program to find the libraries when it runs.
 export LD_LIBRARY_PATH=${CONDA_ENV_PATH}/lib/:$LD_LIBRARY_PATH
 # The two variables are used to tell make how to find the gsl, openmp when compiling the program.
-# Note we install armadillo with brew, we test conda install armadillo, but it seems has some problems
+# Note we install armadillo with brew. In fact, we test conda install armadillo, but it has some problems
 # when compile. Help is needed.
 export CONDA_ENV_INCLUDE=${CONDA_ENV_PATH}/include/
 export CONDA_ENV_LIB=${CONDA_ENV_PATH}/lib/
 # export LD_RUN_PATH=${CONDA_ENV_PATH}/lib/:$LD_RUN_PATH
 
-# Even in macOS, we use g++ to compile (not clnag).
+# Even in macOS, we use g++ to compile (not clang).
 # In makefile, we use g++, so if you have no g++ in your local bin path,
 # you can use soft link below. (Ideally, after brewinng install gcc, it will
-# link gcc to the brew installed dir, i.e., /usr/local/Cellar/gcc).
+# link gcc to the installed dir by brew, i.e., /usr/local/Cellar/gcc).
 ln -s /usr/local/Cellar/gcc/9.2.0/bin/g++-9 /usr/local/bin/g++
 
 # Now, compile the program.
